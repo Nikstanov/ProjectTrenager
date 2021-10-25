f69d0d0eb01e4758e304b3e9ea2c38578afa0cc9
Nikstanov <N.stahnov@gmail.com>
2021-10-25 18:17:48 +0300

    да

--- src/main/java/com/epicteam/projecttrenager/Main.java
+++ src/main/java/com/epicteam/projecttrenager/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             arraylevels[i] = Integer.parseInt(scan.nextLine());
         }
         fr.close();
+        scan.close();
         Application.launch(args);
     }
 
