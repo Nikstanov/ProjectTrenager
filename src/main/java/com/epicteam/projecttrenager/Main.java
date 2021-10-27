eb40c80fedf1a9a725b322ca48aa172968705c20
ItPavel <savenapavel@gmail.com>
2021-10-27 21:48:08 +0300

    Changing experience

--- src/main/java/com/epicteam/projecttrenager/Main.java
+++ src/main/java/com/epicteam/projecttrenager/Main.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 
 public class Main extends Application {
-    static int exp = 0;
+    static int exp = 1;
     static int[] arraylevels = {0,0,0,0,0,0,0,0,0,0};
 
     public static void main(String[] args) throws Exception{
